c0002a6d1089324edd33f7f2db891081969b0d54
Rasmus <liltorp03@gmail.com>
2025-03-18 18:07:52 +0100

    setup

---
+++ OMS-Test/Program.cs
@@ -0,0 +1,43 @@
+using OMS_Test.Components;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Add circuit options configuration
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.DetailedErrors = true; // Only in development
+});
+
+// Add Blazor Bootstrap
+builder.Services.AddBlazorBootstrap();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    // Enable detailed errors in development
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
